816aae0c1552097ec19136d89f4dc0502da31ef8
Hussein135-coder <80627987+Hussein135-coder@users.noreply.github.com>
2024-08-07 11:52:32 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const axios = require("axios");
 const app = express();
 const PORT = 3000;
 //const URL = "https://3000-01hzkq91ad81aw7qbwnsgv5z49.cloudspaces.litng.ai";
-const URL = "https://3000-01j2xwnz7cs7ha5c61w2ag9d37.cloudspaces.litng.ai"
+// https://3000-01j4mnabzqzzhe1ataf9h9nqnd.cloudspaces.litng.ai   SYM4
+// https://3000-01j2xwnz7cs7ha5c61w2ag9d37.cloudspaces.litng.ai   alimm3
+const URL = "https://3000-01j4mnabzqzzhe1ataf9h9nqnd.cloudspaces.litng.ai"
 const URL2 = "https://3000-01j24d27h0nxf71kx65wdhjp7y.cloudspaces.litng.ai";
 
 app.get("/", (req, res) => {
