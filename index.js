546043816ad11778ca924cd10f129de2c2cc7c1a
Hussein135-coder <80627987+Hussein135-coder@users.noreply.github.com>
2024-09-02 18:00:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const PORT = 3000;
 //const URL = "https://3000-01hzkq91ad81aw7qbwnsgv5z49.cloudspaces.litng.ai";
 // https://3000-01j4mnabzqzzhe1ataf9h9nqnd.cloudspaces.litng.ai   SYM4
 // https://3000-01j2xwnz7cs7ha5c61w2ag9d37.cloudspaces.litng.ai   alimm3
-const URL = "https://3000-01j4mnabzqzzhe1ataf9h9nqnd.cloudspaces.litng.ai"
-const URL2 = "https://3000-01j24d27h0nxf71kx65wdhjp7y.cloudspaces.litng.ai";
+const URL = "https://3000-01j6sh23c4ec48ea24vezyd2fr.cloudspaces.litng.ai" //Saleh
+const URL2 = "https://3000-01j24d27h0nxf71kx65wdhjp7y.cloudspaces.litng.ai"; //SYm3
 
 app.get("/", (req, res) => {
   res.send("Express server is running.");
