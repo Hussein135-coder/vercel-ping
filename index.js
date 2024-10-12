5d7395a60a33b94f33401c767c29ad39807b275f
Hussein135-coder <80627987+Hussein135-coder@users.noreply.github.com>
2024-10-12 12:36:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = 3000;
 //const URL = "https://3000-01hzkq91ad81aw7qbwnsgv5z49.cloudspaces.litng.ai";
 // https://3000-01j4mnabzqzzhe1ataf9h9nqnd.cloudspaces.litng.ai   SYM4
 // https://3000-01j2xwnz7cs7ha5c61w2ag9d37.cloudspaces.litng.ai   alimm3
-const URL = "https://3000-01j6sh23c4ec48ea24vezyd2fr.cloudspaces.litng.ai" //Saleh
+const URL = "https://3000-01j7qsc5xgtbyn0m635xb2d6bv.cloudspaces.litng.ai" //Saleh
 const URL2 = "https://3000-01j24d27h0nxf71kx65wdhjp7y.cloudspaces.litng.ai"; //SYm3
 
 app.get("/", (req, res) => {
